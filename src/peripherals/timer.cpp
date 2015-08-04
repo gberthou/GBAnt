9557be63cdda43a868e45b615652df6ddf2357d8
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-08-04 23:19:29 +0200

    Added timer triggering

--- src/peripherals/timer.cpp
+++ src/peripherals/timer.cpp
@@ -1,8 +1,13 @@
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+
 #include "timer.h"
 
 Timer::Timer(u_int32_t bAddress, u_int32_t eAddress):
 	Memory(bAddress, eAddress)
 {
+	memset(channels, 0, sizeof(channels));
 }
 
 Timer::~Timer()
@@ -11,10 +16,21 @@ Timer::~Timer()
 
 bool Timer::Write(u_int32_t address, u_int32_t value, MemoryAccess memAccess)
 {
-	(void) value;
-	(void) memAccess;
 	if(address >= bAddress && address <= eAddress)
 	{
+		if(memAccess == MA16)
+		{
+			u_int16_t *data = reinterpret_cast<u_int16_t*>(channels);
+			data[(address - bAddress) / 2] = value;
+		}
+		else // MA32
+		{
+			u_int32_t *data = reinterpret_cast<u_int32_t*>(channels);
+			data[(address - bAddress) / 4] = value;
+		}
+
+		Trigger();
+
 		return true;
 	}
 	return false;
@@ -22,12 +38,33 @@ bool Timer::Write(u_int32_t address, u_int32_t value, MemoryAccess memAccess)
 
 bool Timer::Read(u_int32_t address, u_int32_t &value, MemoryAccess memAccess)
 {
-	(void) value;
-	(void) memAccess;
 	if(address >= bAddress && address <= eAddress)
 	{
+		if(memAccess == MA16)
+		{
+			u_int16_t *data = reinterpret_cast<u_int16_t*>(channels);
+			value = data[(address - bAddress) / 2];
+		}
+		else // MA32
+		{
+			u_int32_t *data = reinterpret_cast<u_int32_t*>(channels);
+			value = data[(address - bAddress) / 4];
+		}
 		return true;
 	}
 	return false;
+}
+
+void Timer::Trigger(void)
+{
+	for(unsigned int i = 0; i < TIMER_CHANNELS_COUNT; ++i)
+	{
+		if((channels[i].ctrl >> 7) & 1)
+		{
+			std::cout << "TIMER " << i << " enabled" << std::endl;
+			if((channels[i].ctrl >> 6) & 1)
+				std::cout << "TIMER " << i << " IRQ enabled" << std::endl;
+		}
+	}
 }
 
--- src/peripherals/timer.h
+++ src/peripherals/timer.h
@@ -3,6 +3,14 @@
 
 #include "../core/Memory.h"
 
+#define TIMER_CHANNELS_COUNT 4 
+
+struct TimerChannel
+{
+	u_int16_t counter;
+	u_int16_t ctrl;
+};
+
 class Timer : public Memory
 {
 	public:
@@ -11,6 +19,11 @@ class Timer : public Memory
 
 		virtual bool Write(u_int32_t address, u_int32_t value, MemoryAccess memAccess = MA32);
 		virtual bool Read(u_int32_t address, u_int32_t &value, MemoryAccess memAccess = MA32);
+
+		void Trigger(void);
+	
+	private:
+		TimerChannel channels[TIMER_CHANNELS_COUNT];
 };
 
 #endif
