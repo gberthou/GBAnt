f838aa235a5f7208bf435c1de88a84028103eb6e
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-08-12 22:37:05 +0200

    Added bios interruption routine

--- src/core/GBAcpu.cpp
+++ src/core/GBAcpu.cpp
@@ -7,10 +7,14 @@
 #define GBA_ROM_LOADER_BUF_SIZE 1024
 
 const u_int32_t GBA_BASE_ADDRESS = 0x08000000;
+const u_int32_t GBA_IME          = 0x04000208;
+const u_int32_t GBA_IE           = 0x04000200;
+const u_int32_t GBA_IF           = 0x04000202;
 
 GBAcpu::GBAcpu():
 	ARMcpu(GBA_BASE_ADDRESS)
 {
+	mem.AddMemory(new PhysicalMemory(0x00000018, 0x00003FFF)); // BIOS - 16 kB
 	mem.AddMemory(new PhysicalMemory(0x02000000, 0x0203FFFF)); // WRAM - 256 kB
 	mem.AddMemory(new PhysicalMemory(0x03000000, 0x03007FFF)); // WRAM - 32 kB
 	mem.AddMemory(new PhysicalMemory(0x05000000, 0x050003FF)); // BG - 1 kB
@@ -25,9 +29,16 @@ GBAcpu::GBAcpu():
 	mem.AddMemory(new Sound(0x04000060, 0x040000AB));
 	mem.AddMemory(new DMA  (0x040000B0, 0x040000E3, this));
 	mem.AddMemory(new Timer(0x04000100, 0x0400010F));
-	mem.AddMemory(new PhysicalMemory(0x04000110, 0x0400011F)); // Unused
-	mem.AddMemory(new PhysicalMemory(0x0400015A, 0x040001FF)); // Unused
 	mem.AddMemory(new PhysicalMemory(0x04000200, 0x040003FE)); // Interrupt, waitsait
+
+	/* ### Initialize BIOS memory */
+	mem.Write(0x00000018, 0xeafffffe);
+	mem.Write(0x00000128, 0xe92d500f);
+	mem.Write(0x0000012C, 0xe3a00301);
+	mem.Write(0x00000130, 0xe08ee00f);
+	mem.Write(0x00000134, 0xe510f004);
+	mem.Write(0x00000138, 0xe8bd500f);
+	mem.Write(0x0000013C, 0xe25ef004);
 }
 
 GBAcpu::~GBAcpu()
@@ -76,10 +87,30 @@ void GBAcpu::Run(void)
 	ARMcpu::Run();
 }
 
+void GBAcpu::TriggerInterrupt(GBA_InterruptSource source)
+{
+	u_int32_t ie;
+	mem.Read(GBA_IE, ie);
+	if((ie >> source) & 1) // Interrupts are enabled for this particular source
+	{
+		u_int32_t ifvalue;
+		mem.Read(GBA_IF, ifvalue);
+		ifvalue |= (1 << source);
+		mem.Write(GBA_IF, ifvalue); // Update interrupt flags
+	}
+}
+
 void GBAcpu::onClock(void)
 {
 	ARMcpu::onClock();
 	
 	lcd->OnClock();
+}
+
+bool GBAcpu::interruptsEnabled(void)
+{
+	u_int32_t ime;
+	mem.Read(GBA_IME, ime);
+	return (ime & 1) != 0;
 }
 
--- src/core/GBAcpu.h
+++ src/core/GBAcpu.h
@@ -4,6 +4,24 @@
 #include "ARMcpu.h"
 #include "../peripherals/peripherals.h"
 
+enum GBA_InterruptSource
+{
+	GBA_IS_LCD_VBLANK,
+	GBA_IS_LCD_HBLANK,
+	GBA_IS_LCD_VCOUNTER_MATCH,
+	GBA_IS_TIMER0,
+	GBA_IS_TIMER1,
+	GBA_IS_TIMER2,
+	GBA_IS_TIMER3,
+	GBA_IS_SERIAL,
+	GBA_IS_DMA0,
+	GBA_IS_DMA1,
+	GBA_IS_DMA2,
+	GBA_IS_DMA3,
+	GBA_IS_KEYPAD,
+	GBA_IS_GAMEPAK
+};
+
 class GBAcpu : public ARMcpu
 {
 	public:
@@ -13,8 +31,11 @@ class GBAcpu : public ARMcpu
 		bool LoadGBA(const char *filename);		
 		virtual void Run(void);
 
+		void TriggerInterrupt(GBA_InterruptSource source);
+
 	protected:
 		virtual void onClock(void);
+		bool interruptsEnabled(void);
 
 		PhysicalMemory *cartridge;
 		
